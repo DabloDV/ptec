1f1e7d7a474ab3c10052695f0733277bbef21513
DabloDV <juanpabloduquev@hotmail.com>
2025-10-25 09:14:54 -0500

    (monorepo): cambio de password

--- orders-api/src/config.js
+++ orders-api/src/config.js
@@ -8,7 +8,7 @@ db: {
 host: process.env.DB_HOST || 'localhost',
 port: Number(process.env.DB_PORT || 3306),
 user: process.env.DB_USER || 'root',
-password: process.env.DB_PASSWORD || 'XXXX',
+password: process.env.DB_PASSWORD || 'changeme',
 database: process.env.DB_NAME || 'jelou'
 },
 jwtSecret: process.env.JWT_SECRET || 'dev-secret',
